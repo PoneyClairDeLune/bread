7318d52495190d9acfc63680d786c8141af99014
Lumière Élevé <88174309+PoneyClairDeLune@users.noreply.github.com>
2023-04-29 20:55:29 +0000

    Benchmark file fix.

--- sh/allbench.sh
+++ sh/allbench.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-echo "Algorithm		Encode (ms)	Decode (ms)" > benchmark.tsv
+echo "Algorithm	Encode (ms)	Decode (ms)" > benchmark.tsv
 cat conf/algo.txt | while IFS= read -r algo; do
 deno run dist/bench.js $algo >> benchmark.tsv
 done
